571a20f4fb15aedf770f1399339dd7c381a3811b
Prasad7218 <yerawarprasad18@gmail.com>
2022-10-11 19:12:21 +0530

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,35 @@
+import "./Home.css"
+import Axios from 'axios';
+import { useEffect,useState } from "react";
+
+
+
+
+const Home=()=>{
+    const [goods,setGoods]=useState([]);
+
+    useEffect(()=>{
+        Axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{
+            console.log(res.data);
+            setGoods(res.data);
+        }).catch((err)=>{
+            console.log(err);
+        })
+    },[])
+    return(
+        <>
+        {
+        goods.map(ele=>(
+           <div key={"ele"}>
+            <h1>{ele.title}</h1>
+            <p>{ele.body}</p>
+          </div>
+               
+                
+
+        ))}
+        </>
+    )
+}
+
+export default Home;
